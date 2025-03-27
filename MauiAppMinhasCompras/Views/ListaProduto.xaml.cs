3d647d8bdfdb80b4ea3cc14c7aa313ce74523baa
anlauraas <Ana Laura - Recepo@DESKTOP-S3RDG7Q>
2025-03-27 16:32:53 -0300

    Agenda 3 D.S. III

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,3 +1,6 @@
+using MauiAppMinhasCompras.Models;
+using System.Collections.ObjectModel;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
@@ -5,5 +8,16 @@ public partial class ListaProduto : ContentPage
 	public ListaProduto()
 	{
 		InitializeComponent();
+	}
+	private void ToolbarItem_Clicked(object sender, EventArgs e)
+	{
+		try
+		{
+            Navigation.PushAsync(new Views.NovoProduto());
+        }
+		catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "OK");
+		}
 	}
 }
